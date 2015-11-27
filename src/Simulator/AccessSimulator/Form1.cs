0179cd1fa7bb5af1ab1dc0a23d2a2f8355b6c0b2
Evgeny Shmanev <shmanev@evriqum.ru>
2015-11-27 19:04:32 +0300

    access simulator / check access

--- src/Simulator/AccessSimulator/Form1.cs
+++ src/Simulator/AccessSimulator/Form1.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Configuration;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -7,6 +6,7 @@ using AccessControl.Contracts;
 using AccessControl.Contracts.Commands.Lists;
 using AccessControl.Contracts.Dto;
 using AccessControl.Contracts.Helpers;
+using AccessSimulator.AccessCheckServiceProxy;
 using MassTransit;
 
 namespace AccessSimulator
@@ -24,9 +24,35 @@ namespace AccessSimulator
 
         private void button1_Click(object sender, EventArgs e)
         {
+            if (accessPointCombo.SelectedItem == null)
+            {
+                MessageBox.Show("Please select an access point");
+                return;
+            }
+
+            if (userCombo.SelectedItem == null)
+            {
+                MessageBox.Show("Please select a user");
+                return;
+            }
+
+            var accessPoint = ((AccessPointItem) accessPointCombo.SelectedItem);
+            var userHash = ((UserItem)userCombo.SelectedItem).Id;
+            var proxy = new AccessCheckServiceClient();
+            if (proxy.TryPass(new CheckAccess {AccessPointId = accessPoint.Id, UserHash = userHash}))
+            {
+                MessageBox.Show("Welcome to " + accessPoint.Name, "Authorized", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("You are not authorized!", "Not authorized", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+            }
         }
 
-        
+        private void button2_Click(object sender, EventArgs e)
+        {
+            FillData();
+        }
 
         private async void FillData()
         {
@@ -46,6 +72,17 @@ namespace AccessSimulator
             {
                 Invoke(new MethodInvoker(() => userCombo.Items.Add(new UserItem(item))));
             }
+        }
+
+        private static string SiteName(string site)
+        {
+            var firstIndex = site.IndexOf("=");
+            var lastIndex = site.IndexOf(",");
+            if (firstIndex >= 0 && lastIndex > firstIndex)
+            {
+                firstIndex += 1;
+            }
+            return site.Substring(firstIndex, lastIndex - firstIndex);
         }
 
         private class AccessPointItem
@@ -70,20 +107,6 @@ namespace AccessSimulator
 
             public string Id { get; }
             public string Name { get; }
-        }
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-            FillData();
-        }
-
-        private static string SiteName(string site)
-        {
-            var firstIndex = site.IndexOf("=");
-            var lastIndex = site.IndexOf(",");
-            if (firstIndex >= 0 && lastIndex > firstIndex)
-                firstIndex += 1;
-            return site.Substring(firstIndex, lastIndex - firstIndex);
         }
     }
 }
