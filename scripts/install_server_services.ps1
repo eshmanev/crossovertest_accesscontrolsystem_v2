ba704b27b028ea5fb6e24a18c11e84dc0fadbd4f
Evgeny Shmanev <shmanev@evriqum.ru>
2015-12-02 13:35:18 +0300

    server deployment

--- scripts/install_server_services.ps1
+++ scripts/install_server_services.ps1
@@ -79,14 +79,84 @@ function InstallAndRun($servicePath)
   }
 }
 
+function AskParameter($message, $error)
+{
+  $value = Read-Host -Prompt $message
+  if (!$value)
+  {
+    Write-Host "[ERROR]`t " + $error
+    Exit 1
+  }
+  return $value
+}
+
+function LoadConfig($file)
+{
+  $path = [System.IO.Path]::GetFullPath($file)
+  $config = New-Object System.Xml.XmlDocument
+  $config.Load($path)
+  return $config
+}
+
+function ModifyConfigRabbit($file, $url, $user, $password, $nodePath)
+{
+  if (!$nodePath)
+  {
+    $nodePath = "configuration/service/rabbitMq";
+  }
+
+  $config = LoadConfig -file $file
+  $node = $config.SelectSingleNode($nodePath)
+  $node.Attributes["url"].Value = $url
+  $node.Attributes["userName"].Value = $user
+  $node.Attributes["password"].Value = $password
+  $config.Save($file)
+}
+
+function ModifyConfigLDAP($file, $url, $user, $password)
+{
+  $config = LoadConfig -file $file
+  $node = $config.SelectSingleNode('configuration/ldap')
+  $node.Attributes["ldapPath"].Value = $url
+  $node.Attributes["userName"].Value = $user
+  $node.Attributes["password"].Value = $password
+  $config.Save($file)
+}
+
+function ModifyConnectionString($file, $value)
+{
+  $config = LoadConfig -file $file
+  $node = $config.SelectSingleNode("configuration/connectionStrings/add[@name = 'AccessControlSystem']")
+  $node.Attributes["connectionString"].Value = $value
+  $config.Save($file)
+}
+
+# LDAP parameters
+$ldapAddress = AskParameter -message "Enter the LDAP directory server address (Example: LDAP://192.168.1.201)" -error "LDAP directory address is required"
+$ldapUserName = AskParameter -message "Enter the LDAP directory username" -error "LDAP username is required"
+$ldapPassword = AskParameter -message "Enter the LDAP directory password" -error "LDAP password is required"
+# RabbitMQ parameters
+$rabbitAddress = AskParameter -message "Enter the RabbitMQ server address (Example: rabbitmq://192.168.1.201)" -error "RabbitMQ server address is required"
+$rabbitUserName = AskParameter -message "Enter the RabbitMQ username (Example: rabbitmq://192.168.1.201)" -error "RabbitMQ username is required"
+$rabbitPassword = AskParameter -message "Enter the RabbitMQ password (Example: rabbitmq://192.168.1.201)" -error "RabbitMQ password is required"
+# Database parameters
+$connectionString = AskParameter -message "Enter the database connection string (Example: 'Data Source=.\SQLEXPRESS;Initial Catalog=AccessControlSystem;Integrated Security=True')" -error "Connection string is required"
+
+# Do work
 Unzip "AccessControl.Service.LDAP.zip" ".\AccessControl.Service.LDAP"
+ModifyConfigLDAP -file ".\AccessControl.Service.LDAP\AccessControl.Service.LDAP.exe.config" -url $ldapAddress -user $ldapUserName -password $ldapPassword
+ModifyConfigRabbit -file ".\AccessControl.Service.LDAP\AccessControl.Service.LDAP.exe.config" -url $rabbitAddress -user $rabbitUserName -password $rabbitPassword
 InstallAndRun(".\AccessControl.Service.LDAP\AccessControl.Service.LDAP.exe")
 
 Unzip "AccessControl.Service.Notifications.zip" ".\AccessControl.Service.Notifications"
+ModifyConfigRabbit -file ".\AccessControl.Service.Notifications\AccessControl.Service.Notifications.exe.config" -url $rabbitAddress -user $rabbitUserName -password $rabbitPassword
 InstallAndRun(".\AccessControl.Service.Notifications\AccessControl.Service.Notifications.exe")
 
 Unzip "AccessControl.Service.AccessPoint.zip" ".\AccessControl.Service.AccessPoint"
+ModifyConfigRabbit -file ".\AccessControl.Service.AccessPoint\AccessControl.Service.AccessPoint.exe.config" -url $rabbitAddress -user $rabbitUserName -password $rabbitPassword
+ModifyConnectionString -file ".\AccessControl.Service.AccessPoint\AccessControl.Service.AccessPoint.exe.config" -value $connectionString
 InstallAndRun(".\AccessControl.Service.AccessPoint\AccessControl.Service.AccessPoint.exe")
 
 Unzip "AccessControl.Web.zip" ".\AccessControl.Web"
+ModifyConfigRabbit -file ".\AccessControl.Web\web.config" -url $rabbitAddress -user $rabbitUserName -password $rabbitPassword -nodePath "configuration/rabbitMq"
 DeployWebsite -pool "AccessControl.Web" -dotNetVersion "v4.0" -appName "AccessControl.Web" -path ".\AccessControl.Web" -port 8967
